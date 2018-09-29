512a237f0553b8d9146019498cc4292d68bd41ef
swyoo_vmware <tjddnr1360@naver.com>
2018-09-29 20:06:19 +0900

    at home

--- src/swyoo/basic.java
+++ src/swyoo/basic.java
@@ -42,13 +42,14 @@ class basic{
 // If AAA is an instance of BBB or a class that BBB extends or
 // If AAA is an instance that implements BBB directly or indirectly, it returns True! 
 // we can know about abstract class if we look into p381
+/*
 interface Printable {
 	void print(String doc); // abstract method have to declare only.
 	//default void printCMYK(String doc){}
 }
 
 class PrnA implements Printable{
-	//@Override
+	@Override
 	public void print(String doc){
 		System.out.println("From A printer");
 		System.out.println(doc);
@@ -56,11 +57,17 @@ class PrnA implements Printable{
 }
 
 class PrnB implements Printable{
-	//@Override
+	@Override
 	public void print(String doc){
 		System.out.println("From B printer");
 		System.out.print(doc);
 	}
+	
+	//@Override
+	//public void printCMYK(String doc){
+	//	System.out.println("From color printer");
+	//	System.out.println(doc);
+	//}
 	
 }
 
@@ -75,10 +82,126 @@ class basic{
 		prn2.print(myDoc);
 	}
 }
+*/
 
+// Exception from Programmer 
+// a class that extends Exception must execute exception through throws or try, catch ! 
+// throws meaning : p404, Rule for Exception class : p402
+/*
+import java.util.Scanner;
 
+class UserError extends Exception {
+	public UserError(){ super(" it is not available to use the input ");}
+}
+class basic {
+	public static void main(String[] args){
+		System.out.print("input your age: ");
+		
+		try{
+			int age = readAge();
+			System.out.printf("entered age : %d \n", age);
+		}
+		catch(UserError e){
+			System.out.println(e.getMessage());
+		}
+	}
+	public static int readAge() throws UserError {
+		Scanner kb = new Scanner(System.in);
+		int age = kb.nextInt();
+		
+		if(age<0)
+			throw new UserError();
+		return age;
+	}
+}
+*/
 
+// Deep copy 
+// in order to copy an instance deeply, we need to override Object.clone() function
+// , and we need to implement Cloneable for marker interface  
+class Point implements Cloneable {
+    private int xPos;
+    private int yPos;
+    
+    public Point(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+    
+    public void showPosition() {
+        System.out.printf("[%d, %d]", xPos, yPos);
+        System.out.println();
+    }
 
+    public void changePos(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+    
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
 
+class Rectangle implements Cloneable {
+    private Point upperLeft;     // ÁÂÃø »óŽÜ ÁÂÇ¥
+    private Point lowerRight;     // ¿ìÃø ÇÏŽÜ ÁÂÇ¥
+    
+    public Rectangle(int x1, int y1, int x2, int y2) {
+        upperLeft = new Point(x1, y1);
+        lowerRight = new Point(x2, y2);
+    }
+
+    // ÁÂÇ¥ Á€ºžžŠ ŒöÁ€ÇÔ
+    public void changePos(int x1, int y1, int x2, int y2) {
+        upperLeft.changePos(x1, y1);
+        lowerRight.changePos(x2, y2);
+    }
+    
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        // Object Å¬·¡œºÀÇ clone žÞŒÒµå È£Ãâ °á°úžŠ ŸòÀœ
+        Rectangle copy = (Rectangle)super.clone();
+
+        // ±íÀº º¹»çÀÇ ÇüÅÂ·Î º¹»çº»À» ŒöÁ€
+        copy.upperLeft = (Point)upperLeft.clone();
+        copy.lowerRight = (Point)lowerRight.clone();
+        
+        // ¿ÏŒºµÈ º¹»çº»ÀÇ ÂüÁ¶žŠ ¹ÝÈ¯
+        return copy;        
+    }
+
+    // Á÷»ç°¢Çü ÁÂÇ¥ Á€ºž Ãâ·Â    
+    public void showPosition() {
+        System.out.print("ÁÂÃø »óŽÜ: ");
+        upperLeft.showPosition();
+        
+        System.out.print("¿ìÃø ÇÏŽÜ: ");
+        lowerRight.showPosition();
+        System.out.println(); 
+    }
+}
+
+class basic {
+    public static void main(String[] args) {
+        Rectangle org = new Rectangle(1, 1, 9, 9);
+        Rectangle cpy;
+        
+        try {
+            // ÀÎœºÅÏœº º¹»ç
+            cpy = (Rectangle)org.clone();
+
+            // ÀÎœºÅÏœºÀÇ ÁÂÇ¥ Á€ºžžŠ ŒöÁ€
+            org.changePos(2, 2, 7, 7);
+
+            org.showPosition();
+            cpy.showPosition();
+        }
+        catch(CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 
